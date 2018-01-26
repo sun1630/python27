0dc7aa8127c4627f73491269361e9886a0989cfc
sun1630 <sun1630@126.com>
2018-01-26 17:28:40 +0800

    add a file xiaomi_beautifulsoup.py

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 # -*- coding:utf-8 -*-
 
+# api 接口处理类
 from flask import Flask,request
 from flask_cors import CORS
 
@@ -8,7 +9,7 @@ app=Flask(__name__)
 # 允许 hw请求 跨域  http://flask-cors.readthedocs.io/en/latest/
 CORS(app,resources=({r"/hw/*": {"origins": "*"}}))
 
-# GET
+# GET 测试
 # 方式一 get http://localhost:1222/hw?num1=1&num2=2
 @app.route('/hw',methods=['GET','POST'])
 def index():
@@ -20,6 +21,9 @@ def index():
 @app.route('/hw/<num1>/<num2>',methods=['GET','POST'])
 def index1(num1,num2):
     return (num1+num2)
+
+
+
 
 
 if __name__ == "__main__":
---
+++ xiaomi_beautifulsoup.py
@@ -0,0 +1,60 @@
+# -*- coding=utf-8 -*-
+import requests
+import json
+from bs4 import BeautifulSoup
+from collections import OrderedDict
+
+# beautifulSoup4 处理小米商城秒杀HTML
+
+# 抓取小米商城页面HTML
+try:
+    res = requests.get("https://list.mi.com")
+    res.encoding='utf-8'
+    print(res.text)
+except BaseException:
+    print('api Error：接口地址异常')
+    raise Exception("api Error：接口地址异常")
+
+
+# 查找商品分类节点
+soup = BeautifulSoup(res.text, 'html.parser', from_encoding='utf-8')
+categorys=soup.find_all('div',class_='xm-plain-box category-list')
+
+
+shop=[]
+
+for cg in categorys:
+    title = cg.div.h2.get_text()
+    ts = OrderedDict()
+    ts["title"]=title
+
+    shops =[]
+    sps=cg.find_all('li')
+    for sp in sps:
+        spName=sp.find('a',class_='category-list-title').get_text()
+        ss=OrderedDict()
+        ss["shopName"]=spName
+        shops.append(ss)
+
+    ts["shopList"]=shops
+
+    shop.append(ts)
+# 打印数组出现中文编码问题 使用json.dumps([],ensure_ascii=False)
+#print(json.dumps(shop,ensure_ascii=False,indent=4))
+print(json.dumps(shop,ensure_ascii=False,indent=4))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
