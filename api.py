1a0cf487a0cd59d6135d15f8ce6d0c32f28c5447
sun1630 <sun1630@126.com>
2018-01-29 11:04:18 +0800

    Get the merchandise classification and list of Xiaomi Mall

--- api.py
+++ api.py
@@ -3,11 +3,15 @@
 # api 接口处理类
 from flask import Flask,request
 from flask_cors import CORS
+from xiaomi_beautifulsoup import xmApi
+
+
 
 app=Flask(__name__)
 
 # 允许 hw请求 跨域  http://flask-cors.readthedocs.io/en/latest/
-CORS(app,resources=({r"/hw/*": {"origins": "*"}}))
+#CORS(app,resources=({r"*": {"origins": "*"}}))
+CORS(app,resources=({r"/api/*": {"origins": "*"}}))
 
 # GET 测试
 # 方式一 get http://localhost:1222/hw?num1=1&num2=2
@@ -22,9 +26,10 @@ def index():
 def index1(num1,num2):
     return (num1+num2)
 
-
-
-
+# 获取小米商城商品列表
+@app.route('/api/xm',methods=['GET','POST'])
+def xm():
+   return xmApi()
 
 if __name__ == "__main__":
     app.run(debug=True)
--- xiaomi_beautifulsoup.py
+++ xiaomi_beautifulsoup.py
@@ -7,43 +7,52 @@ from collections import OrderedDict
 # beautifulSoup4 处理小米商城秒杀HTML
 
 # 抓取小米商城页面HTML
-try:
-    res = requests.get("https://list.mi.com")
-    res.encoding='utf-8'
-    print(res.text)
-except BaseException:
-    print('api Error：接口地址异常')
-    raise Exception("api Error：接口地址异常")
+def xmUrl():
+    res=False
+    try:
+        res = requests.get("https://list.mi.com")
+        res.encoding='utf-8'
+        #print(res.text)
+    except BaseException:
+        print('api Error：接口地址异常')
+        raise Exception("api Error：接口地址异常") #抛出异常欣喜
+    return res
 
 
-# 查找商品分类节点
-soup = BeautifulSoup(res.text, 'html.parser', from_encoding='utf-8')
-categorys=soup.find_all('div',class_='xm-plain-box category-list')
 
+# 查找商品分类节点 获取商品信息
+def shopInfoByCategory(res):
+    soup = BeautifulSoup(res.text, 'html.parser', from_encoding='utf-8')
+    categorys=soup.find_all('div',class_='xm-plain-box category-list')
+    shop=[]
+    for cg in categorys:
+        title = cg.div.h2.get_text()
+        ts = OrderedDict()
+        ts["title"]=title
 
-shop=[]
+        shops =[]
+        sps=cg.find_all('li')
+        for sp in sps:
+            spName=sp.find('a',class_='category-list-title').get_text()
+            ss=OrderedDict()
+            ss["shopName"]=spName
+            shops.append(ss)
 
-for cg in categorys:
-    title = cg.div.h2.get_text()
-    ts = OrderedDict()
-    ts["title"]=title
+        ts["shopList"]=shops
+        shop.append(ts)
+        # 打印数组出现中文编码问题 使用json.dumps([],ensure_ascii=False)
+        # print(json.dumps(shop,ensure_ascii=False,indent=4))
+    #print(json.dumps(shop, ensure_ascii=False, indent=4))
+    return json.dumps(shop, ensure_ascii=False)
 
-    shops =[]
-    sps=cg.find_all('li')
-    for sp in sps:
-        spName=sp.find('a',class_='category-list-title').get_text()
-        ss=OrderedDict()
-        ss["shopName"]=spName
-        shops.append(ss)
+# 入口接入
+def xmApi():
+    res=xmUrl()
+    json=shopInfoByCategory(res)
+    return json
 
-    ts["shopList"]=shops
-
-    shop.append(ts)
-# 打印数组出现中文编码问题 使用json.dumps([],ensure_ascii=False)
-#print(json.dumps(shop,ensure_ascii=False,indent=4))
-print(json.dumps(shop,ensure_ascii=False,indent=4))
-
-
+if __name__=='__main__':
+    xmApi()
 
 
 
